28bb0ecb51f489b3321f078e950bd63a2af51264
gorno <xalivan@ukr.net>
2021-09-11 15:37:50 +0300

    1.Start

---
+++ src/main/java/com/example/mostvaluableplayer/MostValuablePlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mostvaluableplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostValuablePlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MostValuablePlayerApplication.class, args);
+    }
+
+}
